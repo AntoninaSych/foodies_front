5c6c9b15b4bffcee1f54ce605703c8687f124a2a
Roman Onishchenko <roman.onishchenko@onelog.ch>
2025-05-01 11:04:13 +0300

    rename typo in selectors (#36)

--- src/redux/ingredients/selectors.js
+++ src/redux/ingredients/selectors.js
@@ -1,3 +1,3 @@
-export const selectCategories = state => state.ingredients.items;
+export const selectIngredients = state => state.ingredients.items;
 export const selectLoading = state => state.ingredients.loading;
 export const selectError = state => state.ingredients.error;
--- src/redux/testimonials/selectors.js
+++ src/redux/testimonials/selectors.js
@@ -1,3 +1,3 @@
-export const selectCategories = state => state.testimonials.items;
+export const selectTestimonials = state => state.testimonials.items;
 export const selectLoading = state => state.testimonials.loading;
 export const selectError = state => state.testimonials.error;
