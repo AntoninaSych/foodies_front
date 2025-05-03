37948d129f03642610320141015df839c18e871c
Polina Petrenko <147924269+PolinaPetrenko@users.noreply.github.com>
2025-05-03 16:54:56 +0200

    Merge latest develop into logo-nav

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import styles from './Logo.module.css';
+
+const Logo = () => {
+  return (
+    <Link to="/" className={styles.logo}>
+      foodies
+    </Link>
+  );
+};
+
+export default Logo;
