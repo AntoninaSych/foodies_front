3261d8e5dc9358d1be4b0848bc5231c3f322fcdb
Vasyl-Gumeniuk <vasyl.gumeniuk@gmail.com>
2025-05-03 14:25:50 +0300

    feat(hero): added Hero section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,11 @@
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import Button from '../../components/Button/Button';
 import Container from '../../components/Container/Container';
-import { ROUTERS } from '../../const';
 import { fetchCategories } from '../../redux/categories/operations';
 import CategoriesList from '../../components/CategoriesList/CategoriesList';
 import css from './HomePage.module.css';
 import AppBar from '../../components/AppBar/AppBar';
+import Hero from '../../components/Hero/Hero';
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -20,19 +19,7 @@ const HomePage = () => {
       <div className={css.header}>
         <Container>
           <AppBar theme="dark" />
-          <div className={css.hero}>
-            <h1 className={css.title}>Improve Your Culinary Talents</h1>
-            <h2 className={css.description}>
-              Amazing recipes for beginners in the world of cooking, enveloping
-              you in the aromas and tastes of various cuisines.
-            </h2>
-            <Button
-              to={ROUTERS.ADD_RECIPE}
-              variant={Button.variants.SECONDARY_REVERSED}
-            >
-              Add recipe
-            </Button>
-          </div>
+          <Hero />
         </Container>
       </div>
       <Container>
