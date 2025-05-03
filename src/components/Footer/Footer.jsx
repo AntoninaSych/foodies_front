5fa75e145848e47b0682d167769e0ae0a886f7a5
Dansport <bogdan.onoyko@gmail.com>
2025-05-03 12:29:48 +0200

    feat(footer): add responsive footer with social icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,32 @@
-import Container from "../Container/Container";
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import { Facebook, Instagram, Twitter } from '@mui/icons-material';
 
-import css from "./Footer.module.css";
+import css from './Footer.module.css';
 
 const Footer = () => {
-  const year = (new Date()).getFullYear();
+  const year = new Date().getFullYear();
 
   return (
     <footer className={css.footer}>
-      <Container>
-        <div className={css.wrapper}>@{year}, Foodies. All rights reserved</div>
+      <Container className={css.container}>
+        <div className={css.wrap_footer}>
+          <div className={css.logo}>foodies</div>
+
+          <div className={css.list_social}>
+            <IconButton className={css.item_social} aria-label="Facebook">
+              <Facebook className={css.icon} />
+            </IconButton>
+            <IconButton className={css.item_social} aria-label="Instagram">
+              <Instagram className={css.icon} />
+            </IconButton>
+            <IconButton className={css.item_social} aria-label="Twitter">
+              <Twitter className={css.icon} />
+            </IconButton>
+          </div>
+        </div>
+
+        <div className={css.copr}>© {year} Foodies. All rights reserved</div>
       </Container>
     </footer>
   );
