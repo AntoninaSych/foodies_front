d67d5f01c3f48d204232ba10e094c22b7702c047
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2025-05-04 10:30:50 +0200

    fix: recipes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,32 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import Container from '../../components/Container/Container';
+
 import { fetchCategories } from '../../redux/categories/operations';
+import { fetchIngredients } from '../../redux/ingredients/operations';
+import { fetchAreas } from '../../redux/areas/operations';
+
+import { selectIngredients } from '../../redux/ingredients/selectors';
+import { selectAreas } from '../../redux/areas/selectors';
+
 import CategoriesList from '../../components/CategoriesList/CategoriesList';
-import css from './HomePage.module.css';
 import AppBar from '../../components/AppBar/AppBar';
 import Hero from '../../components/Hero/Hero';
 import Recipes from '../../components/Recipes/Recipes';
+
+import css from './HomePage.module.css';
 
 const HomePage = () => {
   const dispatch = useDispatch();
   const [category, setCategory] = useState('desserts');
 
+  const ingredients = useSelector(selectIngredients);
+  const areas = useSelector(selectAreas);
+
   useEffect(() => {
     dispatch(fetchCategories());
+    dispatch(fetchIngredients());
+    dispatch(fetchAreas());
   }, [dispatch]);
 
   const onChangeCategory = category => {
@@ -36,7 +49,12 @@ const HomePage = () => {
         {!category ? (
           <CategoriesList onChangeCategory={onChangeCategory} />
         ) : (
-          <Recipes category={category} onBack={handleBack} />
+          <Recipes
+            category={category}
+            onBack={handleBack}
+            ingredients={ingredients}
+            areas={areas}
+          />
         )}
       </Container>
     </div>
