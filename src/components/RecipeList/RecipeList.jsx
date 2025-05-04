708fa2d5fffd9a098f371b6ed377a0ff41ecab89
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2025-05-04 21:56:46 +0200

    fix: recipelist

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -1,14 +1,24 @@
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
-import { RecipeCard } from '../RecipeCard/RecipeCard';
-import { selectFavoriteRecipes } from '../../redux/recipes/selectors';
+import {
+  selectRecipes,
+  selectLoading,
+  selectError,
+  selectFavoriteRecipes,
+} from '../../redux/recipes/selectors';
 import { getFavoriteRecipes } from '../../redux/recipes/operations';
 
 import css from './RecipeList.module.css';
+import RecipeCard from '../RecipeCard/RecipeCard';
+import Loader from '../Loader/Loader';
+import Message from '../Message/Message';
 
-const RecipeList = ({ recipes }) => {
+const RecipeList = () => {
   const dispatch = useDispatch();
+  const recipes = useSelector(selectRecipes);
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
   const favoriteRecipes = useSelector(selectFavoriteRecipes);
 
   useEffect(() => {
@@ -20,17 +30,25 @@ const RecipeList = ({ recipes }) => {
     return favoriteRecipes.some(fav => fav.recipeId === id);
   };
 
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    <ul className={css['recipes-list']}>
-      {recipes.map(recipe => (
-        <li key={recipe.id || recipe._id} className={css['recipes-item']}>
+    <div className={css.wrapper}>
+      {error && <Message>{error}</Message>}
+      {recipes.length === 0 ? (
+        <p>No recipes found.</p>
+      ) : (
+        recipes.map(recipe => (
           <RecipeCard
+            key={recipe.id}
             recipe={recipe}
-            isFavorite={isFavorite(recipe.id || recipe._id)}
+            isFavorite={isFavorite(recipe.id)}
           />
-        </li>
-      ))}
-    </ul>
+        ))
+      )}
+    </div>
   );
 };
 
