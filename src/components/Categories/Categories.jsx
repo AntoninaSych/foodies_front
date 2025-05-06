a5a6d3a2c0bd1f73b4115a96920a205105a8a0f9
Roman Onishchenko <orm81zp@gmail.com>
2025-05-06 22:56:56 +0300

    fix: some categories and recipes updates (#74)

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,19 +1,30 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchCategories } from '../../redux/categories/operations';
 import MainTitle from '../../components/MainTitle/MainTitle';
 import Subtitle from '../../components/Subtitle/Subtitle';
 import CategoriesList from '../../components/CategoriesList/CategoriesList';
-import Button from '../Button/Button';
-
-import styles from './Categories.module.css';
+import {
+  selectCategories,
+  selectError,
+  selectLoading,
+} from '../../redux/categories/selectors';
+import Loader from '../Loader/Loader';
+import Message from '../Message/Message';
 
 const Categories = ({ handleChangeCategory }) => {
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+  const categories = useSelector(selectCategories);
 
   useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <section>
@@ -23,8 +34,14 @@ const Categories = ({ handleChangeCategory }) => {
         recipes that combine taste, style and the warm atmosphere of the
         kitchen.
       </Subtitle>
-      <CategoriesList handleChangeCategory={handleChangeCategory} />
-      <Button className={styles.btnLoadAll}>All categories</Button>
+      {error ? (
+        <Message>{error}</Message>
+      ) : (
+        <CategoriesList
+          handleChangeCategory={handleChangeCategory}
+          categories={categories}
+        />
+      )}
     </section>
   );
 };
