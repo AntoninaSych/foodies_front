f6bb81d54fdf14cbdc1ea83316ea04783ad0b87c
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2025-05-07 22:07:04 +0200

    feat: add RecipeInfo

--- src/components/RecipeMainInfo/RecipeMainInfo.jsx
+++ src/components/RecipeMainInfo/RecipeMainInfo.jsx
@@ -1,22 +1,27 @@
 import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { ROUTERS } from '../../const';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
-import { openModal } from '../../redux/modal/slice';
+import SignInModal from '../SignInModal/SignInModal';
 
 import css from './RecipeMainInfo.module.css';
+import { useState } from 'react';
 
 const RecipeMainInfo = ({ title, category, time, description, recipe }) => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   const handleAuthorClick = () => {
     if (isLoggedIn) {
       navigate(`${ROUTERS.USER}/${recipe.owner.id}`);
     } else {
-      dispatch(openModal('signIn'));
+      setIsOpen(true);
     }
+  };
+
+  const handleOnClose = () => {
+    setIsOpen(false);
   };
 
   return (
@@ -41,6 +46,7 @@ const RecipeMainInfo = ({ title, category, time, description, recipe }) => {
           </button>
         )}
       </div>
+      <SignInModal isOpen={isOpen} onClose={handleOnClose} />
     </div>
   );
 };
--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import css from './DetailPage.module.css';
 import { useParams } from 'react-router-dom';
-import { recipesDetailFetch } from '../../api/recipesApi';
+//import { recipesDetailFetch } from '../../api/recipesApi';
 import Container from '../../components/Container/Container';
 import Loader from '../../components/Loader/Loader';
 import Message from '../../components/Message/Message';
@@ -10,9 +10,39 @@ import PathInfo from '../../components/PathInfo/PathInfo';
 import RecipeInfo from '../../components/RecipeInfo/RecipeInfo';
 //import PopularRecipes from '../../components/PopularRecipes/PopularRecipes';
 
+const mokrecipe = {
+  id: '37d2d47b-cb9a-4903-a92c-8c4a8194143b',
+  title: 'Shawarma',
+  description:
+    'A Middle Eastern wrap filled with tender strips of marinated meat',
+  instructions:
+    'Combine the marinade ingredients in a large ziplock bag (or bowl).\r\nAdd the chicken and use your hands to make sure each piece is coated. If using a ziplock bag, I find it convenient to close the bag then massage the bag to disperse the rub all over each chicken piece.\r\nMarinate overnight or up to 24 hours.\r\nCombine the Yoghurt Sauce ingredients in a bowl and mix. Cover and put in the fridge until required (it will last for 3 days in the fridge).\r\nHeat grill/BBQ (or large heavy based pan on stove) on medium high. You should not need to oil it because the marinade has oil in it and also thigh fillets have fat. But if you are worried then oil your hotplate/grill. (See notes for baking)\r\nPlace chicken on the grill and cook the first side for 4 to 5 minutes until nicely charred, then turn and cook the other side for 3 to 4 minutes (the 2nd side takes less time).\r\nRemove chicken from the grill and cover loosely with foil. Set aside to rest for 5 minutes.\r\nTO SERVE\r\nSlice chicken and pile onto platter alongside flatbreads, Salad and the Yoghurt Sauce.\r\nTo make a wrap, get a piece of flatbread and smear with Yoghurt Sauce. Top with a bit of lettuce and tomato and Chicken Shawarma. Roll up and enjoy!',
+  thumb: 'http://localhost:5000/public/images/recipies/Shawarma.jpg',
+  time: '24',
+  areaId: '104f50b1-1efe-4ab7-8e1b-ead3a2020988',
+  ownerId: 'd26b9a3f-81ed-401e-9011-9d03dfac52db',
+  categoryId: '17163542-9eb1-4f45-849c-3a60a72d442e',
+  createdAt: '2025-05-07T19:22:10.655Z',
+  updatedAt: '2025-05-07T19:22:10.655Z',
+  area: {
+    id: '104f50b1-1efe-4ab7-8e1b-ead3a2020988',
+    name: 'Egyptian',
+  },
+  owner: {
+    id: 'd26b9a3f-81ed-401e-9011-9d03dfac52db',
+    name: 'Foodies Admin',
+    email: 'admin@foodies.com',
+  },
+  category: {
+    id: '17163542-9eb1-4f45-849c-3a60a72d442e',
+    name: 'Chicken',
+    thumb: 'images/categories/Chicken.jpg',
+  },
+};
+
 const DetailPage = () => {
   const { id } = useParams();
-  const [recipe, setRecipe] = useState(null);
+  const [recipe] = useState(mokrecipe);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
@@ -21,8 +51,8 @@ const DetailPage = () => {
       try {
         setLoading(true);
 
-        const recipeData = await recipesDetailFetch(id);
-        setRecipe(recipeData);
+        // const recipeData = await recipesDetailFetch(id);
+        // setRecipe(recipeData);
 
         // const popularData = await fetchPopularRecipes();
       } catch {
@@ -32,7 +62,9 @@ const DetailPage = () => {
       }
     };
 
-    if (id) fetchData();
+    if (id) {
+      fetchData();
+    }
   }, [id]);
 
   if (loading) return <Loader />;
@@ -41,7 +73,7 @@ const DetailPage = () => {
   return (
     <section className={css.wrapper}>
       <Container>
-        <PathInfo breadcrumbs={[{ name: 'Recipe' }]} />
+        <PathInfo breadcrumbs={[{ name: recipe.title }]} />
         <RecipeInfo recipe={recipe} />
 
         {/*<PopularRecipes recipes={popularRecipes}  />*/}
