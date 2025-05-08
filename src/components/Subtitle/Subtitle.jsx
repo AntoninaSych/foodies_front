b7d7ca3383c94bd13292f52fc0d04c5050691134
Roman Onishchenko <roman.onishchenko@onelog.ch>
2025-05-08 11:26:46 +0300

    fix: update subtitle

--- src/components/Subtitle/Subtitle.jsx
+++ src/components/Subtitle/Subtitle.jsx
@@ -1,7 +1,8 @@
+import clsx from 'clsx';
 import css from './Subtitle.module.css';
 
 const Subtitle = ({ children, className }) => {
-  return <p className={`${css.text} ${className}`}>{children}</p>;
+  return <p className={clsx(css.text, className)}>{children}</p>;
 };
 
 export default Subtitle;
