6031749be38640e13a58cc9638b44c6deacbaddb
Roman Onishchenko <roman.onishchenko@onelog.ch>
2025-05-08 13:31:10 +0300

    chore: update modals

--- src/components/SignInModal/SignInModal.jsx
+++ src/components/SignInModal/SignInModal.jsx
@@ -1,21 +1,13 @@
 import SignInForm from '../SignInForm/SignInForm';
-import Link from '../Link/Link';
 import Modal from '../Modal/Modal';
 import css from '../styles/modal.module.css';
 
-const SignInModal = ({ isOpen, onClose, handleOnChangeForm }) => {
+const SignInModal = ({ isOpen, onClose }) => {
   return (
     <Modal open={isOpen} onClose={onClose}>
       <div className={css.wrapper}>
         <h2 className={css.title}>SIGN IN</h2>
         <SignInForm onSuccess={onClose} />
-
-        {handleOnChangeForm && (
-          <div className={css.footer}>
-            <span>Don&#39;t have an account?</span>{' '}
-            <Link onClick={handleOnChangeForm}>Create an account</Link>
-          </div>
-        )}
       </div>
     </Modal>
   );
