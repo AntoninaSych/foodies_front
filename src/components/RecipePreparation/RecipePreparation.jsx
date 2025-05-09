6efb76a3ad821343afc2155a444a5762243fbd50
Roman Onishchenko <roman.onishchenko@onelog.ch>
2025-05-10 01:58:10 +0300

    fix: add auth modal to recipe preparation

--- src/components/RecipePreparation/RecipePreparation.jsx
+++ src/components/RecipePreparation/RecipePreparation.jsx
@@ -1,26 +1,28 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import {
   addToFavorites,
+  getFavoriteRecipes,
   removeFromFavorites,
 } from '../../redux/recipes/operations';
 import Button from '../Button/Button';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
 import { selectFavoriteRecipes } from '../../redux/recipes/selectors';
+import { showModal } from '../../redux/common/slice';
+import { MODALS } from '../../const';
 import css from './RecipePreparation.module.css';
 
 const RecipePreparation = ({ recipe }) => {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const favorites = useSelector(selectFavoriteRecipes);
-  if (!recipe) return null;
 
   const isFavorite =
     Array.isArray(favorites) && favorites.some(fav => fav.id === recipe.id);
 
   const handleFavoriteClick = () => {
     if (!isLoggedIn) {
-      // TODO SHOW AUTH MODAL
-      // return dispatch(showModal(MODALS.AUTH));
+      return dispatch(showModal(MODALS.AUTH));
     }
 
     if (isFavorite) {
@@ -29,9 +31,16 @@ const RecipePreparation = ({ recipe }) => {
       dispatch(addToFavorites(recipe.id));
     }
   };
+
   const paragraphs = recipe.instructions
     .split(/\r?\n\s*\r?\n/)
     .filter(p => p.trim() !== '');
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(getFavoriteRecipes());
+    }
+  }, [dispatch, isLoggedIn]);
 
   return (
     <div className={css.wrapper}>
